e4b9adb1ff222b7241cefe293b966c43778d7de9
LeticiaCoelho <Leticia.ac23@gmail.com>
2019-01-21 20:09:43 -0200

    Lib padrão

---
+++ Pesquisa/lib/VSAT.cpp
@@ -0,0 +1,19 @@
+/*
+  Arduino VSAT Lib
+
+  Functions created in this library:
+
+  "TX_Lora"     ->  LoraWan Transmiter
+  "RX_Lora"     ->  LoraWan Receptor
+
+  Created : January 21, 2019
+  Author: Leticia Coelho 
+*/
+
+#include <vsat.h>
+
+int VSAT::TX_Lora(int data_to_transmit); /* LoraWan Transmiter */
+       
+int VSAT::RX_Lora(); /* LoraWan Receptor */
+        
+
