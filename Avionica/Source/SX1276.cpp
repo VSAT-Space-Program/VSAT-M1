d0eee4871265c64ba03a9c3b888fd75079a092c1
educampos <educampos28@yahoo.com.br>
2019-01-23 22:08:55 -0500

    Revisões na inicialização

--- Avionica/Source/SX1276.cpp
+++ Avionica/Source/SX1276.cpp
@@ -23,19 +23,46 @@ SX1276::~SX1276() {
 
 bool SX1276::Initialize(Usart* Serial){
 
+	//Attach the serial port
+	this->Serial = Serial;
+
 	DDRD |= (1<<M0) | (1<<M1); //Set M0 and M1 port as output
 
 	//Put the device in sleep mode to configure it.
 	Sleep();
 
+	//Initial configuration message
 	uint8_t msg[6];
-
+	//Default configuration
 	msg[0]= 0xc0;
 	msg[1]= ADDH;
 	msg[2]= ADDL;
+	msg[3]= AIR_BPS_2400 | USART_BPS_9600 |P_8N1;
+	msg[4]= CHAN;
+	msg[5]= 0b01000100;
+
+	uint8_t command[3] = {0xc1,0xc1,0xc1};
+	this->Serial->writeBytes(command, 3);
+
+	uint8_t output[6];
+	//TODO - a porta serial não possui uma função para fazer a leitura de n bytes,
+	//this->Serial->readBytes(output,6);
+
+	//Verify if the LoRa module has the right configuration
+	bool aux= true;
+	for(uint8_t idx=0;idx<sizeof(msg);idx++)
+	{
+		aux = aux & (msg[idx] == output[idx]);
+	}
+
+	if(aux==false)
+	{
+		//There is something different in the configuration
+		//TODO - Send the right configuration
+	}
+
 
 	//TODO- Finish configuration
-
 
 	Normal();
 
@@ -47,10 +74,16 @@ bool SX1276::Initialize(Usart* Serial){
 		_delay_ms(1);
 		Counter++;
 		if (Counter>=1000)
-			return false; //fail to resert the module
+			return false; //fail to start the module
 	}
 
 
-	return true;
+	return true; //successful
+}
+
+bool SX1276::Send(uint16_t Adrress, uint8_t Channel, uint8_t* data, uint8_t Lenght){
+
+
+
 }
 
--- Avionica/Source/SX1276.h
+++ Avionica/Source/SX1276.h
@@ -18,9 +18,9 @@
 
 // Arduino Tx PD0 ---> SX1276 Rx
 // Arduino Rx PD1 <--- SX1276 Tx
-// Arduino ?? PD2 ---> SX1276 M0
-// Arduino ?? PD3 ---> SX1276 M1
-// Arduino ?? PD4 <--- SX1276 AUX
+// Arduino PD2 ---> SX1276 M0
+// Arduino PD3 ---> SX1276 M1
+// Arduino PD4 <--- SX1276 AUX
 
 
 #define M0 PD2
@@ -28,17 +28,39 @@
 #define AUX PD4
 
 #define ADDH 0x00
-#define ADDL 0x01
+#define ADDL 0x00
 
 //Operating Frequency
-//0x00 to 0xFF corresponding to 900 to 931 MHz
-//Default 915 MHz
-#define CHAN
+//0x00 to 0x1F corresponding to 900 to 931 MHz
+
+#define CHAN 0x0F  //Default 915 MHz
+
+//Air data rate
+#define AIR_BPS_300 	0x00
+#define AIR_BPS_1200 	0x01
+#define AIR_BPS_2400 	0x02
+#define AIR_BPS_4800 	0x03
+#define AIR_BPS_9600 	0x04
+#define AIR_BPS_19300 	0x05
+
+//USART data rate
+#define USART_BPS_1200 	 0x00
+#define USART_BPS_2400 	 0x08
+#define USART_BPS_4800 	 0x10
+#define USART_BPS_9600 	 0x18
+#define USART_BPS_19300  0x20
+#define USART_BPS_38400  0x28
+#define USART_BPS_57600  0x30
+#define USART_BPS_115200 0x38
+
+//UART parity bit
+#define P_8N1 0x00
 
 class SX1276 {
 public:
 	SX1276();
 	virtual ~SX1276();
+	bool Send(uint16_t Adrress, uint8_t Channel, uint8_t* data, uint8_t Lenght);
 
 	bool Initialize(Usart* Serial);
 
