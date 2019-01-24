7b57ff2f0c46001bcfb1571eee06c2819e1a6b4e
educampos <educampos28@yahoo.com.br>
2019-01-23 22:09:43 -0500

    Classe para o GPS

---
+++ Avionica/Source/NEO6M.cpp
@@ -0,0 +1,18 @@
+/*
+ * NEO6M.cpp
+ *
+ *  Created on: 20 de jan de 2019
+ *      Author: educampos
+ */
+
+#include "NEO6M.h"
+
+NEO6M::NEO6M() {
+	// TODO Auto-generated constructor stub
+
+}
+
+NEO6M::~NEO6M() {
+	// TODO Auto-generated destructor stub
+}
+
