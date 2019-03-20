4f00889c3f0635eef2078b010912a9ce74640563
Joao <ioannesbrito@gmail.com>
2019-03-20 16:25:18 -0300

    DS3231
    CLASSE PARA O RTC-DS3231

--- Avionica/Source/DS3231.h
+++ Avionica/Source/DS3231.h
@@ -22,6 +22,7 @@
 #define CONV_TEMPERATURE_FORCE_BIT 			0x20
 #define CONV_TEMPERATURE_FORCE_BUSY_BIT 	0x08
 
+
 class DS3231 {
 public:
 	DS3231();
@@ -32,6 +33,27 @@ public:
 	double Get_Temperature();
 	bool Force_Temperature_Update();
 	bool Adjust_Time(const DateTime& dt);
+	 byte readControlByte(bool which);
+
+
+	void enableOscillator(bool TF, bool battery, byte frequency);
+			// turns oscillator on or off. True is on, false is off.
+			// if battery is true, turns on even for battery-only operation,
+			// otherwise turns off if Vcc is off.
+			// frequency must be 0, 1, 2, or 3.
+			// 0 = 1 Hz
+			// 1 = 1.024 kHz
+			// 2 = 4.096 kHz
+			// 3 = 8.192 kHz (Default if frequency byte is out of range);
+		void enable32kHz(bool TF);
+			// Turns the 32kHz output pin on (true); or off (false).
+		bool oscillatorCheck();;
+			// Checks the status of the OSF (Oscillator Stop Flag);.
+			// If this returns false, then the clock is probably not
+			// giving you the correct time.
+			// The OSF is cleared by function setSecond();.
+
+
 
 	uint8_t data[6];
 	uint8_t &ss=data[0];
@@ -42,6 +64,12 @@ public:
 	uint8_t &m=data[5];
 
 private:
+    byte readControlByte(bool which);
+			// Read selected control byte: (0); reads 0x0e, (1) reads 0x0f
+    void writeControlByte(byte control, bool which);
+			// Write the selected control byte.
+			// which == false -> 0x0e, true->0x0f.
+
 	TwoWire* Wire;
 
 	uint8_t Temp;
