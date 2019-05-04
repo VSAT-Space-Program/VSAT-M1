b158084803102d323da5fffcf63d48952d8990c7
Joao <ioannesbrito@gmail.com>
2019-05-04 15:13:41 -0300

    Revisão - MS5607 Altimetro

---
+++ Avionica/Source/MS5607.cpp
@@ -0,0 +1,214 @@
+/*
+  * MS5607.cpp
+    *  Created on: 30 de marco de 2019
+        Author: joaobrito
+  */
+
+#include "MS5607.h"
+#include <Wire.h>
+
+MS5607::MS5607()
+{
+
+}
+
+
+uint8_t MS5607::Initialize(TwoWire* Wire){
+
+  this->Wire = Wire;
+  readCalibration();
+  return 1;
+}
+
+uint8_t MS5607::resetDevice(void){
+  Wire->beginTransmission(MS5607_ADDRESS);
+  Wire->write(RESET);
+  uint8_t error = Wire->endTransmission();
+  if(error == 0){
+  _delay_ms(4);
+    return(1);
+  }else{return(0);}
+}
+
+uint8_t MS5607::readCalibration(){
+
+  if(resetDevice() &&
+    readUInt_16(PROM_READ+2, C1) &&
+    readUInt_16(PROM_READ+2*2, C2) &&
+    readUInt_16(PROM_READ+2*3, C3) &&
+    readUInt_16(PROM_READ+2*4, C4) &&
+    readUInt_16(PROM_READ+2*5, C5) &&
+    readUInt_16(PROM_READ+2*6, C6)
+  ){
+    #ifdef MS5607_DEBUG
+    Serial.print("C1 - ");
+    Serial.println(C1);
+    Serial.print("C2 - ");
+    Serial.println(C2);
+    Serial.print("C3 - ");
+    Serial.println(C3);
+    Serial.print("C4 - ");
+    Serial.println(C4);
+    Serial.print("C5 - ");
+    Serial.println(C5);
+    Serial.print("C6 - ");
+    Serial.println(C6);
+    #endif
+
+    return (1);
+  }else{return(0);}
+}
+
+int16_t MS5607::readUInt_16(uint8_t address, uint16_t &value){
+  uint8_t data[2]; //4bit
+  data[0] = address;
+  if (readBytes(data,2))
+  {
+    value = (((unsigned int)data[0]*(1<<8))|(unsigned int)data[1]);
+    return(1);
+  }
+  value = 0;
+  return(0);
+}
+
+uint8_t MS5607::readBytes(uint8_t *values, uint8_t length){
+  uint8_t x;
+
+    Wire->beginTransmission(MS5607_ADDRESS);
+    Wire->write(values[0]);
+
+  uint8_t error = Wire->endTransmission();
+  if (error == 0)
+  {
+    Wire->requestFrom((uint8_t)MS5607_ADDRESS,length);
+    while(!Wire->available()) ; // wait until bytes are ready
+    for(x=0;x<length;x++)
+    {
+      values[x] = Wire->read();
+    }
+    return(1);
+  }
+  return(0);
+}
+
+uint8_t MS5607::startConversion(uint8_t CMD){
+  Wire->beginTransmission(MS5607_ADDRESS);
+  Wire->write(CMD);
+  uint8_t error = Wire->endTransmission();
+  if(error == 0){
+    switch (OSR) {
+      case 256:
+      _delay_ms(1);
+        break;
+      case 4096:
+      _delay_ms(10);
+        break;
+      default:
+        break;
+    }
+    return(1);
+  }else{return(0);}
+}
+
+uint8_t MS5607::startMeasurment(void){
+  Wire->beginTransmission(MS5607_ADDRESS);
+  Wire->write(R_ADC);
+  uint8_t error = Wire->endTransmission();
+  if(error == 0){
+  _delay_ms(3);
+    return 1;
+  }else{return 0;}
+}
+
+uint8_t MS5607::getDigitalValue(void){
+    if(startConversion(CONV_D1)){
+      if(startMeasurment()){
+        if(readDigitalValue(DP));
+      }
+      }else{return 0;}
+    if(startConversion(CONV_D2)){
+      if(startMeasurment()){
+        if(readDigitalValue(DT));
+      }
+    }else{return 0;}
+   #ifdef MS5607_DEBUG
+  Serial.print("D1 - ");
+  Serial.println(DP);
+  Serial.print("D2 - ");
+  Serial.println(DT);
+  #endif
+  return 1;
+}
+
+uint8_t MS5607::readDigitalValue(uint32_t &value){
+  uint8_t x, length = 3;
+  unsigned char data[3];
+
+  Wire->requestFrom((uint8_t)MS5607_ADDRESS,length);
+
+  while(!Wire->available()) ; // wait until bytes are ready
+
+  for(x=0;x<length;x++)
+  {
+    data[x] = Wire->read();
+  }
+
+  value = (uint32_t)data[0]*1<<16|(uint32_t)data[1]*1<<8|(uint32_t)data[2];
+
+  return(1);
+  }
+
+  double MS5607::getTemperature(void){
+
+      // Calculate Temperature.
+
+      // First Order Temperature Compensation.
+      dT = DT - C[5]*pow(2,8);
+      TEMP = (2000.0 +( dT * C[6])/pow(2,23));
+
+      if (TEMP < 2000.0){
+
+        T2 = dT*dT/pow(2,31); // Second Order Temperature Compensation.
+      }
+      TEMP -= T2;
+      return TEMP/100;
+
+
+
+  }
+
+
+  double MS5607::getPressure(bool compensation){
+
+      OFF = C[2]*pow(2,17)+(C[4]*dT)/pow(2,6);
+      SENS = C[1]*pow(2,16)+(C[3]*dT)/pow(2,7);
+
+      if (compensation) //First Order Compensation.
+      {
+          TEMP = (2000.0 +( dT * C[6])/pow(2,23));
+
+          OFF2 = 0;
+          SENS2 = 0;
+      }
+      if (TEMP < 2000.0) //Second Order Compensation.
+      {
+          OFF2 = 61*((TEMP-2000)*(TEMP-2000))/pow(2,4);
+          SENS2 = 2*((TEMP-2000)*(TEMP-2000));
+
+      }
+      if (TEMP < -1500.0) //Third Order Compensation.
+      {
+          OFF2 += 15*((TEMP+1500)*(TEMP+1500));
+          SENS2 += 8*((TEMP+1500)*(TEMP+1500));
+      }
+
+        //Calculate Pressure.
+        OFF-= OFF2;
+        SENS -= SENS2;
+
+        P = (((DP*SENS)/pow(2,21)-OFF)/pow(2,15));
+
+        return P/100;
+  }
+
+
---
+++ Avionica/Source/MS5607.h
@@ -0,0 +1,48 @@
+	/*
+	* MS5607.h
+		*  Created on: 30 de marco de 2019
+	      Author: joaobrito
+	*/
+
+#define MS5607_H
+
+#include<Wire.h>
+
+#define MS5607_ADDRESS 0X76
+#define PROM_READ  0xA0
+#define RESET  0x1E
+#define OSR 4096
+//#define CONV_D1  0x40 //OSR = 256 //calculation time = 0.60 ms
+#define CONV_D1 0x48 //OSR = 4096 //calculation time = 9.04 ms
+//#define CONV_D2  0x50 //OSR = 256 //calculation time = 0.60 ms
+#define CONV_D2 0x58 //OSR=4096 //calculation time = 9.04 ms
+#define R_ADC  0X00 //adc read
+
+
+class MS5607
+{
+  public:
+    public:
+    MS5607();
+    uint8_t begin();
+    double getTemperature(void);
+    double getPressure(void);
+    uint8_t getDigitalValue(void);
+
+  private:
+    uint16_t C[6];
+    unsigned int C1,C2,C3,C4,C5,C6;
+    unsigned long DP, DT;
+    double dT, TEMP, P, T2;
+    int64_t OFF, SENS, OFF2, SENS2;
+
+    uint8_t resetDevice(void);
+    uint8_t readCalibration();
+    int16_t readUInt_16(uint8_t address, unsigned int &value);
+    uint8_t readBytes(unsigned char *values, uint8_t length);
+    uint8_t startConversion(uint8_t CMD);
+    uint8_t startMeasurment(void);
+    uint8_t readDigitalValue(unsigned long &value);
+    String ToString(uint64_t x);
+
+};
